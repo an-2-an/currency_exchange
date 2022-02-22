48892b9e4235d23eddbd8a111524b242d31dd35f
an-2-an <rjabenkoae@gmail.com>
2022-02-22 14:24:16 +0200

    two

--- currency_exchange.py
+++ currency_exchange.py
@@ -2,20 +2,25 @@ class CurrencyExchanger:
 
     def __init__(self):
         self.amount = None
+        self.rates = {
+            'ARS': 0.82,
+            'HNL': 0.17,
+            'AUD': 1.9622,
+            'MAD': 0.208,
+        }
 
     def get_currency_amount(self):
         return float(input('Please, enter the number of mycoins you have: > '))
 
-    def get_currency_exchange_rate(self):
-        return float(input('Please, enter the exchange rate: > '))
+    # def get_currency_exchange_rate(self):
+    #     return float(input('Please, enter the exchange rate: > '))
 
     def show_result(self):
-        print(f'The total amount of dollars: {self.amount}')
+        for curr, rate in self.rates.items():
+            print(f'I will get {round(self.amount * rate, 2):.2f} {curr} from the sale of {self.amount:.2f} mycoins.')
 
     def start(self):
-        c = self.get_currency_amount()
-        r = self.get_currency_exchange_rate()
-        self.amount = round(c * r, 2)
+        self.amount = self.get_currency_amount()
         self.show_result()
 
 if __name__ == '__main__':
